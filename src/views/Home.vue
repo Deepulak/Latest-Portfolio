8aadc839e5562edb730b3bfe84778576dcce683e
@Deecoder <45496136+Deepulak@users.noreply.github.com>
2022-04-17 20:18:51 +0600

    first commit
    there is the dees-space first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,204 @@
+<template>
+  <v-layout column justify-center align-center class="mt-4 pt-2">
+    <VueCompareImage
+      class="hidden-md-and-down"
+      hover
+      :style="{ minWidth: '1300px' }"
+      :sliderLineWidth="sliderLine"
+      :handleSize="hSize"
+      :leftImage="leftImage"
+      :rightImage="rightImage"
+      :sliderPositionPercentage="sliderPosition"
+    />
+    <VueCompareImage
+      class="hidden-lg-and-up"
+      hover
+      :style="{ maxWidth: '400px' }"
+      :sliderLineWidth="sliderLine"
+      :handleSize="hSize"
+      :leftImage="leftImage2"
+      :rightImage="rightImage2"
+      :sliderPositionPercentage="sliderPosition"
+    />
+
+    <br>
+
+    <vue-typer class="headline" :repeat="0" text="Dee Pu Lak"></vue-typer>
+    <vue-typer
+      :text="text1"
+      :repeat="Infinity"
+      :shuffle="false"
+      initial-action="erasing"
+      :pre-type-delay="70"
+      :type-delay="70"
+      :pre-erase-delay="1980"
+      :erase-delay="150"
+      erase-style="select-back"
+      :erase-on-complete="false"
+      caret-animation="smooth"
+    ></vue-typer>
+    <br>
+    <v-flex xs12 sm6 md4 lg4 xl4>
+      <v-card flat color="transparent" max-width="500">
+        <v-card-title primary-title>
+          <div>
+            <h3 class="headline mb-0">
+              <span>About</span>
+              <span class="blue--text">Me</span>
+            </h3>
+            <div>
+              <p>
+                Hello! I’m Deepu. I'm a full-stack web developer who
+                is passionate about
+                <span
+                  class="green--text font-weight-bold"
+                ></span> building great user experiences,
+                fighting for simplicity over complexity and constantly learning.
+                I'm also making responsive web apps, ios and android application.
+              </p>
+            </div>
+          </div>
+        </v-card-title>
+
+        <v-card-actions class="hidden-sm-and-down">
+          <v-btn
+            v-for="icon in icons"
+            :key="icon.icon"
+            fab
+            dark
+            outline
+            color="#FF3A3F"
+            :href="icon.href"
+            target="_blank"
+          >
+            <v-icon dark>{{icon.icon}}</v-icon>
+          </v-btn>
+        </v-card-actions>
+
+        <v-card-actions class="hidden-md-and-up justify-center">
+          <v-btn
+            v-for="icon in icons"
+            :key="icon.icon"
+            small
+            fab
+            dark
+            outline
+            color="#FF3A3F"
+            :href="icon.href"
+            target="_blank"
+          >
+            <v-icon dark>{{icon.icon}}</v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import { VueTyper } from "vue-typer";
+import VueCompareImage from "vue-compare-image";
+
+export default {
+  metaInfo: {
+    title: "Home",
+    titleTemplate: "%s ← Dee's Space",
+    meta: [
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { name: "description", content: "Dees's Portfolio" },
+      { charset: "utf-8" },
+      { property: "og:title", content: "Dee's Space" },
+      { property: "og:site_name", content: "Dee's Space" },
+      { property: "og:type", content: "website" },
+      { property: "og:url", content: "https://dees.space" },
+      {
+        property: "og:image",
+        //content: "https://i.imgur.com/Dcz2PGx.jpg"
+        content: "https://cdn.pixabay.com/photo/2018/06/08/00/48/developer-3461405__340.png"
+      },
+      { property: "og:description", content: "Dee's Portfolio" }
+    ]
+  },
+  components: {
+    "vue-typer": VueTyper,
+    VueCompareImage
+  },
+  data() {
+    return {
+      icons: [
+        {
+          href: "https://www.facebook.com/dipu.pulock/",
+          icon: "fab fa-facebook-f"
+        },
+       { href: "https://www.instagram.com/its__elliott__/", icon: "fab fa-instagram" },
+       {
+          href: "https://www.youtube.com/channel/UCbRH_m9qoOxYggiUUgx-j1w",
+          icon: "fab fa-youtube"
+        },
+        {
+          href: "https://www.linkedin.com/in/deepu-pulaak-b84b73175/",
+          icon: "fab fa-linkedin-in"
+        },
+        
+         { href: "", icon: "fab fa-twitter" },
+        {
+          href: "",
+          icon: "fas fa-envelope"
+        },
+        
+      ],
+      text1: ["Full-Stack Developer", "Web Developer", "Web Designer"],
+      //leftImage: "https://i.imgur.com/bU50uS9.jpg",
+      //leftImage: "https://i.pinimg.com/originals/95/cf/9d/95cf9d8936d115c734fedc06185437ab.jpg",
+      //leftImage: "https://cdn.pixabay.com/photo/2021/08/04/13/06/software-developer-6521720__480.jpg",
+      leftImage: "https://wallpaper.dog/large/20512774.png",
+      //rightImage: "https://i.imgur.com/z3ZoEEw.jpg",
+      //rightImage: "https://wallpaper.dog/large/20512774.png",
+      rightImage: "https://cdn.pixabay.com/photo/2021/08/04/13/06/software-developer-6521720__480.jpg",
+      //leftImage2: "https://i.imgur.com/SXBitjh.jpg",
+      leftImage2: "https://wallpaper.dog/large/20512774.png",
+      //rightImage2: "https://i.imgur.com/Dcz2PGx.jpg",
+      //rightImage2: "https://i.pinimg.com/originals/95/cf/9d/95cf9d8936d115c734fedc06185437ab.jpg",
+      rightImage2: "https://cdn.pixabay.com/photo/2018/06/08/00/48/developer-3461405__340.png",
+      sliderLine: 0,
+      hSize: 0,
+      sliderPosition: 0.5
+    };
+  }
+};
+</script>
+<style>
+@keyframes rocking {
+  0%,
+  100% {
+    transform: rotateZ(-10deg);
+  }
+
+  50% {
+    transform: rotateZ(10deg);
+  }
+}
+
+.vue-typer {
+  font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
+  font-size: 25px;
+}
+.vue-typer .custom.char.typed {
+  color: #4a15f7;
+}
+.vue-typer .custom.char.selected {
+  color: #b92401;
+}
+
+.vue-typer .custom.caret {
+  animation: rocking 1s ease-in-out 0s infinite;
+}
+.vue-typer .custom.caret.typing {
+  background-color: green;
+}
+.vue-typer .custom.caret.selecting {
+  display: inline-block;
+  background-color: #e91e63;
+}
+</style>
+
---
+++ src/views/Resume.vue
@@ -0,0 +1,248 @@
+<template>
+  <v-layout row justify-center align-center wrap class="mt-4 pt-2">
+    <v-flex xs12 sm12 md5 lg6 xl6 class="mt-4 pt-4">
+      <h2 class="mb-4 pl-4 pt-2">Education</h2>
+      <v-card-text class="py-0">
+        <v-timeline align-top dense>
+          <v-timeline-item color="#3500D4" small>
+            <v-layout pt-3>
+              <v-flex xs4>
+                <div class="caption font-weight-bold blue--text">JAN 2019 -</div>
+                <div class="caption font-weight-bold blue--text">PRESENT</div>
+              </v-flex>
+              <v-flex class="pl-2">
+                <strong>Full Stack Web Development Course</strong>
+                <div class="caption mb-2">freeCodeCamp</div>
+              </v-flex>
+            </v-layout>
+          </v-timeline-item>
+
+          <v-timeline-item color="#3500D4" small>
+            <v-layout pt-3>
+              <v-flex xs4>
+                <div class="caption font-weight-bold blue--text">JUN 2018 -</div>
+                <div class="caption font-weight-bold blue--text">PRESENT</div>
+              </v-flex>
+              <v-flex class="pl-2">
+                <strong>Master of Arts</strong>
+                <div class="caption mb-2">National Versity of Bangladesh</div>
+              </v-flex>
+            </v-layout>
+          </v-timeline-item>
+
+          <v-timeline-item color="#3500D4" small>
+            <v-layout pt-3>
+              <v-flex xs4>
+                <div class="caption font-weight-bold blue--text">MAY 2017 -</div>
+                <div class="caption font-weight-bold blue--text">SEP 2017</div>
+              </v-flex>
+              <v-flex class="pl-2">
+                <strong>Front-End Developer</strong>
+                <div class="caption mb-2">Edureka</div>
+              </v-flex>
+            </v-layout>
+          </v-timeline-item>
+
+          <v-timeline-item color="#3500D4" small>
+            <v-layout pt-3>
+              <v-flex xs4>
+                <div class="caption font-weight-bold blue--text">MAR 2016 -</div>
+                <div class="caption font-weight-bold blue--text">APR 2017</div>
+              </v-flex>
+              <v-flex class="pl-2">
+                <strong>Higher Seconday Certificate</strong>
+                <div class="caption mb-2">Dr Arifur Rahman Commerce College</div>
+              </v-flex>
+            </v-layout>
+          </v-timeline-item>
+        </v-timeline>
+      </v-card-text>
+    </v-flex>
+    <v-flex xs12 sm12 md4 lg6 xl6 class="mt-4 pt-2">
+      <h2 class="mb-4 pl-4">Experience</h2>
+      <v-card-text class="py-0">
+        <v-timeline align-top dense>
+          <v-timeline-item color="#3500D4" small>
+            <v-layout pt-3>
+              <v-flex xs4>
+                <div class="caption font-weight-bold blue--text">DEC 2016 -</div>
+                <div class="caption font-weight-bold blue--text">PRESENT</div>
+              </v-flex>
+              <v-flex class="pl-2">
+                <strong>Freelance Frontend Developer</strong>
+                <div class="caption mb-2">Remote</div>
+              </v-flex>
+            </v-layout>
+          </v-timeline-item>
+
+          <v-timeline-item color="#3500D4" small>
+            <v-layout pt-3>
+              <v-flex xs4>
+                <div class="caption font-weight-bold blue--text">AUG 2019 -</div>
+                <div class="caption font-weight-bold blue--text">APR 2021</div>
+              </v-flex>
+              <v-flex class="pl-2">
+                <strong>IT Technician</strong>
+                <div class="caption mb-2">MFS-EMMAUS</div>
+              </v-flex>
+            </v-layout>
+          </v-timeline-item>
+        </v-timeline>
+      </v-card-text>
+    </v-flex>
+    <v-layout row justify-center align-center wrap class="mt-4 pt-2">
+      <v-flex xs12 sm12 md4 lg5 xl5 class="mx-2 pt-4">
+        <h2>
+          <span>Design</span>
+
+          <span class="blue--text">Skills</span>
+        </h2>
+        <br>
+        <strong>Photoshop</strong>
+        <v-progress-linear v-model="buffer1" :buffer-value="bufferValue" color="#DA0037" height="16"></v-progress-linear>
+        <strong>Adobe XD</strong>
+        <v-progress-linear v-model="buffer2" :buffer-value="bufferValue" color="#DA0037" height="16"></v-progress-linear>
+        <strong>Zeplin</strong>
+        <v-progress-linear v-model="buffer3" :buffer-value="bufferValue" color="#DA0037" height="16"></v-progress-linear>
+        <strong>Adobe Premiere Pro</strong>
+        <v-progress-linear v-model="buffer4" :buffer-value="bufferValue" color="#DA0037" height="16"></v-progress-linear>
+        <strong>Figma</strong>
+        <v-progress-linear v-model="buffer5" :buffer-value="bufferValue" color="#DA0037" height="16"></v-progress-linear>
+        <strong>Illustrator</strong>
+        <v-progress-linear v-model="buffer6" :buffer-value="bufferValue" color="#DA0037" height="16"></v-progress-linear>
+      </v-flex>
+      <v-flex xs12 sm12 md4 lg5 xl5 class="mx-2 pt-4">
+        <h2>
+          <span>Coding</span>
+          <span class="blue--text">Skills</span>
+        </h2>
+        <br>
+        <strong>VUE/NEXT</strong>
+        <v-progress-linear v-model="buffer1" :buffer-value="bufferValue" color="#4201FF" height="16"></v-progress-linear>
+        <strong>HTML/CSS</strong>
+        <v-progress-linear v-model="buffer2" :buffer-value="bufferValue" color="#4201FF" height="16"></v-progress-linear>
+        <strong>JavaScript</strong>
+        <v-progress-linear v-model="buffer3" :buffer-value="bufferValue" color="#4201FF" height="16"></v-progress-linear>
+        <strong>React Native</strong>
+        <v-progress-linear v-model="buffer4" :buffer-value="bufferValue" color="#4201FF" height="16"></v-progress-linear>
+        <strong>AWS</strong>
+        <v-progress-linear v-model="buffer5" :buffer-value="bufferValue" color="#4201FF" height="16"></v-progress-linear>
+        <strong>TypesScript</strong>
+        <v-progress-linear v-model="buffer6" :buffer-value="bufferValue" color="#4201FF" height="16"></v-progress-linear>
+        <strong>Python</strong>
+        <v-progress-linear v-model="buffer7" :buffer-value="bufferValue" color="#4201FF" height="16"></v-progress-linear>
+        <strong>C</strong>
+        <v-progress-linear v-model="buffer8" :buffer-value="bufferValue" color="#4201FF" height="16"></v-progress-linear>
+        <strong>C++</strong>
+        <v-progress-linear v-model="buffer9" :buffer-value="bufferValue" color="#4201FF" height="16"></v-progress-linear>
+        <strong>Django</strong>
+        <v-progress-linear v-model="buffer10" :buffer-value="bufferValue" color="#4201FF" height="16"></v-progress-linear>
+        <strong>MongoDB</strong>
+        <v-progress-linear v-model="buffer11" :buffer-value="bufferValue" color="#4201FF" height="16"></v-progress-linear>
+        <strong>MySQL</strong>
+        <v-progress-linear v-model="buffer12" :buffer-value="bufferValue" color="#4201FF" height="16"></v-progress-linear>
+      </v-flex>
+      <div class="mt-4 pt-2">
+        <!-- <v-btn
+          href="https://docs.google.com/document/d/1WSR42FEgUahJuR_RArW1wz7TfUBlgj13zekRidmQgs4/edit?usp=sharing"
+          target="_blank"
+          color="#ff3a3f"
+          dark
+          large
+        >
+          Download Resume
+          <v-icon right dark>fas fa-file-download</v-icon>
+        </v-btn>
+        -->
+        <v-btn
+         target="_blank"
+          color="#ff3a3f"
+          dark
+          large
+        >
+          Download Resume
+          <v-icon right dark>fas fa-file-download</v-icon>
+        </v-btn>
+      </div>
+    </v-layout>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Resume",
+    titleTemplate: "%s ← Dee's Space",
+    meta: [
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        name: "description",
+        content:
+          "Vue NUXT HTML CSS JavaScript Design Adobe XD Photoshop Coding Skills Resume Work Web Developer Front-End"
+      },
+      { charset: "utf-8" },
+      { property: "og:title", content: "Dee' Space" },
+      { property: "og:site_name", content: "Dee' Space" },
+      { property: "og:type", content: "website" },
+      { property: "og:url", content: "https://Dee.space" },
+      {
+        property: "og:image",
+        content: "https://i.imgur.com/Dcz2PGx.jpg"
+      },
+      {
+        property: "og:description",
+        content:
+          "Vue NUXT HTML CSS JavaScript Design Adobe XD Photoshop Coding Skills Resume Work Web Developer Front-End"
+      }
+    ]
+  },
+  data() {
+    return {
+      value: 0,
+      buffer1: 0,
+      buffer2: 0,
+      buffer3: 0,
+      buffer4: 0,
+      buffer5: 0,
+      buffer6: 0,
+      buffer7: 0,
+      buffer8: 0,
+      buffer9: 0,
+      buffer10: 0,
+      buffer11: 0,
+      buffer12: 0,
+      bufferValue: 100,
+      interval: 0
+    };
+  },
+  mounted() {
+    this.startBuffer();
+  },
+
+  beforeDestroy() {
+    clearInterval(this.interval);
+  },
+
+  methods: {
+    startBuffer() {
+      this.interval = setInterval(() => {
+        this.buffer1 = 90;
+        this.buffer2 = 85;
+        this.buffer3 = 80;
+        this.buffer4 = 80;
+        this.buffer5 = 90;
+        this.buffer6 = 75;
+        this.buffer7 = 80;
+        this.buffer8 = 60;
+        this.buffer9 = 60;
+        this.buffer10 = 70;
+        this.buffer11 = 80;
+        this.buffer12 = 70;
+      }, 800);
+    }
+  }
+};
+</script>
+<style >
+</style>
+
